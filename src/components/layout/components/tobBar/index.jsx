db10c8a711b9af1ad92eaac9a88de9675f1b7406
chaurasiaravi1118 <chaurasiaravi1118@gmail.com>
2025-02-11 22:08:05 +0530

    code done

--- src/components/layout/components/tobBar/index.jsx
+++ src/components/layout/components/tobBar/index.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react"
 import "./topbar.css"
-const TopBar = (props) => {
+import { GitHubIcon, Logo } from "../../../comman/icons/comman";
+const TopBar = () => {
     const [selectedTheme, setSelectedTheme] = useState("dark");
+    const [openRightDrawer, setOpenRightDrawer] = useState(false)
     const settings = [
         {
             label: 'Git',
             type: 'external-link',
-            link: '#'
+            icon: <GitHubIcon />,
+            link: 'https://github.com/jayasri176/first-react'
         },
         {
             label: 'Theme',
-            type:'drop-down',
+            type: 'right-drawer',
+            icon: <GitHubIcon />,
+            onClick:()=>{setOpenRightDrawer(!openRightDrawer)},
             options: [
                 {
                     label: 'Dark',
@@ -27,48 +32,43 @@ const TopBar = (props) => {
             ]
         }
     ]
-   
+
     return (
         <header className="container-fluid header-wrapper">
             <div className="brand-container">
                 {/* //brand */}
                 <div className="brandIcon">
-                    JS
+                    <Logo />
                 </div>
             </div>
 
             <div className="settings-container" >
                 {
-                    settings?.map((n, i)=><div key={i} className="setting-item" >
+                    settings?.map((n, i) => <div key={i} className="setting-item">
                         {
-                            n?.type === 'external-link'?
-                            <button onClick={()=>{window.open("/","_blank")}} className="git-button">
-                            <img
-                                src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-                                alt="GitHub Logo"
-                                className="github-logo"
-                            />
-                        </button>
-                    
-                           : n?.type === 'drop-down'?
-                            <select
-                                value={selectedTheme}
-                                dataType={selectedTheme}
-                                onChange={(e)=>{setSelectedTheme(e?.target?.value)}}
-                                className="theme-selector">
-                                {n.options.map((option, idx) => (
-                                    <option key={idx} value={option.value}>
-                                        {option.label}
-                                    </option>
-                                ))}
-                            </select>
-                        : null
-                    } 
-                </div>
-            )
-        }
-    </div>
-</header>
+                            n?.type === 'external-link' ?
+                                <a target="_blank" href={n?.link} rel="noreferrer" className="git-button">
+                                    {n?.icon}
+                                </a>
+
+                                : n?.type === 'right-drawer' ?
+                                    <button className="git-button" onClick={()=>{n?.onClick()}}>
+                                        {n?.icon}
+                                    </button>
+                                    : null
+                        }
+                    </div>
+                    )
+                }
+            </div>
+
+
+            {
+                openRightDrawer?
+                <div className="w-full p-4" style={{border:'solid red 1px',position:'fixed', top:0, left:0, width:'100%', height:'100%'}}></div>
+                :null
+            }
+        </header>
     )
 }
 
