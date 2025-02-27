093b0672ad57a78e446ddfe177219d5975eea359
chaurasiaravi1118 <chaurasiaravi1118@gmail.com>
2025-02-27 21:33:39 +0530

    code done

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,41 +1,43 @@
-import React, { useEffect, useState } from "react"
-import SideBar from "./components/sideBar"
-import TopBar from "./components/tobBar"
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom"
-import Home from "../../pages/home";
-import AboutUs from "../../pages/aboutUs";
-import ContactUs from "../../pages/contactUs";
-import Protofolio from "../../pages/protfolio";
-import SubProtfolio from "../../pages/protfolio/subProtfolio";
+import React, { lazy, Suspense, useState } from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+const Home = lazy(() => import("../../pages/home"))
+const AboutUs = lazy(() => import("../../pages/aboutUs"))
+const ContactUs = lazy(() => import("../../pages/contactUs"))
+const Protofolio = lazy(() => import("../../pages/protfolio"))
+const SubProtfolio = lazy(() => import("../../pages/protfolio/subProtfolio"))
+const SideBar = lazy(() => import("./components/sideBar"))
+const TopBar = lazy(() => import("./components/tobBar"))
 const Layout = () => { //arrow function components this following ES6 standard
     const [isHandleSideBar, setIsHandleSideBar] = useState(true);
 
-    const handleConditionalRouting = (pathname)=>{
-        let removedSideBarCheck = ['/about-us', '/protfolio', '/contact-us']?.filter((n)=>  pathname?.includes(n));
-        if(removedSideBarCheck?.length){
+    const handleConditionalRouting = (pathname) => {
+        let removedSideBarCheck = ['/about-us', '/protfolio', '/contact-us']?.filter((n) => pathname?.includes(n));
+        if (removedSideBarCheck?.length) {
             setIsHandleSideBar(false)
-         }else{
+        } else {
             setIsHandleSideBar(true)
-         }
+        }
     }
     return (
         <Router>
-            <TopBar handleConditionalRouting={handleConditionalRouting}/>
-            <main className={`container-fluid ${isHandleSideBar?`p-0 main_layout`:`p-4`}`}>
-            {
-                isHandleSideBar?
-                <SideBar/>
-                :null
-            }
-            <section className={`${isHandleSideBar?`full-main-section`:`container-fluid p-0`}`}>
-            <Routes>
-                  <Route path="/" element={<Home/>} />
-                  <Route path="/about-us" element={<AboutUs/>} />
-                  <Route path="/protfolio" element={<Protofolio/>} />
-                  <Route path="/protfolio/:id" element={<SubProtfolio/>} />
-                  <Route path="/contact-us" element={<ContactUs/>} />
-             </Routes>
-            </section>
+            <TopBar handleConditionalRouting={handleConditionalRouting} />
+            <main className={`container-fluid ${isHandleSideBar ? `p-0 main_layout` : `p-4`}`}>
+                {
+                    isHandleSideBar ?
+                        <SideBar />
+                        : null
+                }
+                <section className={`${isHandleSideBar ? `full-main-section` : `container-fluid p-0`}`}>
+                    <Suspense>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/about-us" element={<AboutUs />} />
+                            <Route path="/protfolio" element={<Protofolio />} />
+                            <Route path="/protfolio/:id" element={<SubProtfolio />} />
+                            <Route path="/contact-us" element={<ContactUs />} />
+                        </Routes>
+                    </Suspense>
+                </section>
             </main>
         </Router>
     )
