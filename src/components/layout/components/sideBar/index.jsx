fd4fd440bbbe38cd6de1571f3343cb8ccea5395b
jayasri176 <jayasrikumar34@gmail.com>
2025-02-27 16:09:58 +0530

    code done

--- src/components/layout/components/sideBar/index.jsx
+++ src/components/layout/components/sideBar/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './sideBar.css';
 const SideBar=()=>{
-   
+    const [active, setActive] = useState("Template store");
+
+    const menuItems = [
+        "Git",
+        "React",
+    ];
     return(
         <aside className='asideWrapper'>
+            <ul>
+        {menuItems.map((item) => (
+          <li
+            key={item}
+            className={`menu-item ${active === item ? "active" : ""}`}
+            onClick={() => setActive(item)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+         
 
         </aside>
     )
