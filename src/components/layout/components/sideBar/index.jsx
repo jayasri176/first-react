77ad51a05f5a0f26021449e4636ebca37c1434b1
chaurasiaravi1118 <chaurasiaravi1118@gmail.com>
2025-03-02 21:01:40 +0530

    code done

--- src/components/layout/components/sideBar/index.jsx
+++ src/components/layout/components/sideBar/index.jsx
@@ -1,46 +1,62 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './sideBar.css';
-const SideBar=()=>{
-    const [active, setActive] = useState("Template store");
+import { GoChevronRight } from 'react-icons/go';
+const SideBar = () => {
+  const [active, setActive] = useState("");
 
-    const menuItems = [
-      {
-        label:'Git',
-        slug:'',
-        subItems:[
-          {
-            label:'Introduction',
-            slug:'',
-            subItems:[
-              
-            ]
-          },
-          {
-            label:'Installation fo Git',
-            slug:'',
-            subItems:[
-              
-            ]
-          }
-        ]
-      }
-    ];
-    return(
-        <aside className='asideWrapper'>
-            <ul>
-        {menuItems.map((item) => (
-          <li
-            key={item}
-            className={`menu-item ${active === item ? "active" : ""}`}
-            onClick={() => setActive(item)}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
-         
+  const menuItems = [
+    {
+      label: 'Git',
+      key: 'git',
+      subItems: [
+        {
+          label: 'Introduction',
+          key: 'git-introduction',
+          subItems: []
+        },
+        {
+          label: 'Installation of Git',
+          key: 'intallation-of-git',
+          subItems: [
 
-        </aside>
-    )
+          ]
+        }
+      ]
+    },
+  ];
+  return (
+    <aside className='asideWrapper'>
+      <MapNavItems list={menuItems} active={active} setActive={setActive} />
+    </aside>
+  )
 }
 export default SideBar;
+
+const MapNavItems = ({ className = '', isChild=false, list, active, setActive }) => {
+  return (
+    <ul className={`navigationContainer ${className}`}>
+      {list.map((item, i) => (
+        <li
+          key={i}
+          className={`${isChild?`childIems`:""}  ${active === item?.key ? "activeClass" : ""}`}
+          onClick={() => setActive(item?.key === active?'':item?.key)}
+        >
+          <div className='listWrapper'>
+            {item?.icon ? <div className='iconClass'></div> : null}
+            <label className='labelClass'>{item?.label}</label>
+            {
+              item?.subItems?.length ?
+                <GoChevronRight className={`arrowClass  ${active === item?.key ? `rotateIcon` : ""}`} />
+                : null
+            }
+          </div>
+          {
+            active === item?.key && item?.subItems?.length ?
+              <MapNavItems className="chilNavitems" isChild={true} list={item?.subItems} active={active} setActive={setActive} />
+              : null
+          }
+        </li>
+      ))}
+    </ul>
+  )
+}
